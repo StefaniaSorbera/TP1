f8bb695ae521c5d51d9a527736ef9eabb38716bd
StefaniaSorbera <sorberastefania@gmail.com>
2025-08-27 16:29:26 -0300

    no
    si

--- Source/MyLittleProject3/Public/Actors/Item.h
+++ Source/MyLittleProject3/Public/Actors/Item.h
@@ -19,19 +19,19 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	UPROPERTY(EditAnywhere)
-	int salud;
+	int Salud;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
-	int experiencia;
+	int Experiencia;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties", meta = (ClampMin = "0.0", ClampMax = "100.0", UIMin = "0.0", UIMax = "100.0", ToolTip = "velocidaditem"))
-	float velocidad;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Properties", meta = (ClampMin = "0.0", ClampMax = "100.0", UIMin = "0.0", UIMax = "100.0", ToolTip = "Velocidaditem"))
+	float Velocidad;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Properties")
-	FString nombreitem;
+	FString Nombreitem;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Properties")
-	FString tipo;
+	FString Tipo;
 
 
 protected:
